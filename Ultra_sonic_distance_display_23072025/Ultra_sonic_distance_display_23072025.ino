44f02186b7f2631531cd50a19e517ee94a9359d2
Diyari-Fariq-M-salih <diyari.m.salih@gmail.com>
2025-07-23 15:26:44 +0200

    Update Ultra_sonic_distance_display_23072025.ino
    Ultrasonic basics of sending the wave, taking the feedback and turning it into distance. using speed of sound.

--- Ultra_sonic_distance_display_23072025/Ultra_sonic_distance_display_23072025.ino
+++ Ultra_sonic_distance_display_23072025/Ultra_sonic_distance_display_23072025.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+//First we define the pins that Trig and Echo are connected to. 
 
+const int trigPin = 9;  
+const int echoPin = 10; 
+
+//Then we declare 2 floats, duration and distance, which will hold the length of the sound wave and how far away the object is. 
+float duration, distance;
+
+void setup(){
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+void loop() {  
+
+  //make sure it US is off
+	digitalWrite(trigPin, LOW);  
+	delayMicroseconds(2);  
+
+  //send waves for 10ms
+	digitalWrite(trigPin, HIGH);  
+	delayMicroseconds(10);
+
+  //turn off for safety  
+	digitalWrite(trigPin, LOW);  
+
+  //pulse function to take the duration of travel
+  duration = pulseIn(echoPin, HIGH);
+
+  // quick Google search for "speed of sound in centimeters per microsecond" will say that it is .0343 c/μS
+  distance = (duration*.0343)/2;
+
+  Serial.print("Distance: ");  
+	Serial.println(distance);  
+	delay(100);  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+
